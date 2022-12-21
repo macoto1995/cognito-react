603487ca8c61316b6f25e3ae323927e9baf84681
macoto1995 <adosute07@gmail.com>
2022-12-21 14:07:15 +0900

    init

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './pages/App'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Login } from './pages/login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  }
+]);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode> 
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      
+      <a href='/login'>Login Page</a>
+    </div>
+  )
+}
+
+export default App
